4c0aa0af112ddd4c3728589323575843109b4244
LatvianLaura <lasens@yahoo.com>
2021-08-30 21:44:13 +0300

    Booking System

---
+++ Booking/src/Booking.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Booking {
+
+    public static void main(String[] args) {
+
+        //1. Owner side (what he can do - add, delete tables/seats, delete booking). 3 options
+        //2. User side ( to book a table; to see reservations according to specific date and time) 2. options
+
+        //Class: Restaurant(with all the tables);
+        // Class : Tables ( with table ID, number of seats, array of reservations, date and time)
+        // Class : Person (name, phone number, number of people, table ID, date and time)
+        // Class: Main (switches 1. ar you an owner/user 2.depends on )
+
+
+    }
+}
